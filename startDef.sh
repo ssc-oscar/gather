e01c6718c3b9264926886d6190b05bfa1d069167
Audris Mockus <audris@utk.edu>
2018-11-27 03:06:54 +0000

    adding collection scripts

--- startDef.sh
+++ startDef.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-i=jovyan
+i=audris
 echo "$i ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$i
 sed -i 's/^$/+ : '$i' : ALL/' /etc/security/access.conf
 /usr/sbin/sshd -e
 cd /home/$i
-sudo -H -u $i sh -c /bin/notebook.sh
 while true
 do echo here $(pwd) $(ps -ef | grep ssh) 
    sleep 15
