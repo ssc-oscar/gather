08cd4e1fdde799c227c9604e178c688c1b37171a
Audris Mockus <audris@utk.edu>
2019-01-27 22:21:41 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-
+if [[ 'a' == 'b' ]];
+then
 strt=0
 for i in {0..5}
 do fr=$(($i*20000000+$strt))
    to=$(($fr+20000000))
-   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201812 $fr $to &> ghReposList201812.$fr-$to &
+   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201813 $fr $to &> ghReposList201812.$fr-$to &
 done
 wait
+fi 
+
 strt=120000000
 for i in {0..4}
 do fr=$(($i*20000000+$strt))
    to=$(($fr+20000000))
-   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201812 $fr $to &> ghReposList201812.$fr-$to &
+   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201813 $fr $to &> ghReposList201812.$fr-$to &
 done
-python3 bbRepos.py 1980-01-01 bitbucket201812 &> bbRepos201812.out
+python3 bbRepos.py 1980-01-01 bitbucket201813 &> bbRepos201813.out &
+
+python3 extractSfGit.py sf201813 repos &
+
+
 wait
 
 #${un[$i]} ${ps[$i]} 
