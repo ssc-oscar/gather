2438c4dbbd77e913389303afd1a1bc8ffa0b3538
Audris Mockus <audris@utk.edu>
2019-01-29 17:07:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,9 +17,36 @@ do fr=$(($i*20000000+$strt))
    to=$(($fr+20000000))
    python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201813 $fr $to &> ghReposList201812.$fr-$to &
 done
+if [[ 'a' == 'b' ]];
+then
+strt=0
+for i in {0..5}
+do fr=$(($i*20000000+$strt))
+   to=$(($fr+20000000))
+   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201813 $fr $to &> ghReposList201812.$fr-$to &
+done
+wait
+
+strt=120000000
+for i in {0..4}
+do fr=$(($i*20000000+$strt))
+   to=$(($fr+20000000))
+   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201813 $fr $to &> ghReposList201812.$fr-$to &
+done
+wait
+
+fi
+
+strt=150000000
+for i in {0..5}
+do fr=$(($i*5000000+$strt))
+   to=$(($fr+5000000))
+   python3 ghReposList.py ${un[$i]} ${ps[$i]} ghReposList201813 $fr $to &> ghReposList201813.$fr-$to &
+done   
+
 python3 bbRepos.py 1980-01-01 bitbucket201813 &> bbRepos201813.out &
 
-python3 extractSfGit.py sf201813 repos &
+(python3 sfRepos.py sf201813 repos; python3 extractSfGit.py sf201813 repos) &
 
 python3 	glRepos.py 1 gl201813 repos &
 
