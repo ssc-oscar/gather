0ab2f5667fe17b9559d8b24e1a1a95b8b9236d6c
Audris Mockus <audris@utk.edu>
2019-04-25 10:39:08 -0400

    Create getHeadsDr.sh

---
+++ getHeadsDr.sh
@@ -0,0 +1,3 @@
+cat drupal.com | sed 's|https://git.drupalcode.org/||;s|^|dr:|;s|\.git$||' | \
+while read r; do git ls-remote $r | grep -E 'HEAD|refs/heads' | perl -ane 's/\s+/;/;print "'$r';$_";'
+done
