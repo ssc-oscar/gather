77567bc0b01ebffc2d24f6eed96e672884556b7f
Audris Mockus <audris@utk.edu>
2019-04-25 10:20:04 -0400

    Update getHeadsGN.sh

--- getHeadsGN.sh
+++ getHeadsGN.sh
@@ -1,3 +1,3 @@
-cat gitlab.gnome.org | sed 's|https://https://gitlab.gnome.org/||;s|^|gl_gnome:|;s|\.git$||' | \
+cat gitlab.gnome.org | sed 's|https://gitlab.gnome.org/||;s|^|gl_gnome:|;s|\.git$||' | \
 while read r; do git ls-remote $r | grep -E 'HEAD|refs/heads' | perl -ane 's/\s+/;/;print "'$r';$_";'
 done
