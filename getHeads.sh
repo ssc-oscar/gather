5c8a6d43603b2382b3b144c69b7d19a00fb55034
Audris Mockus <audris@utk.edu>
2019-04-25 10:18:11 -0400

    Do debian separately

--- getHeads.sh
+++ getHeads.sh
@@ -1,4 +1,4 @@
-for i in git.savannah.gnu.org bioconductor.org.201903 android.googlesource.com git.kernel.org git.zx2c4.com cgit.kde.org git.eclipse.org git.debian.org repo.or.cz drupal.com sf201903.prj git.postgresql.org
+for i in git.savannah.gnu.org bioconductor.org.201903 android.googlesource.com git.kernel.org git.zx2c4.com cgit.kde.org git.eclipse.org repo.or.cz drupal.com sf201903.prj git.postgresql.org
 do cat $i | while read r; do git ls-remote $r | grep -E 'HEAD|refs/heads' | perl -ane 's/\s+/;/;print "'$r';$_";'
 done
 done
